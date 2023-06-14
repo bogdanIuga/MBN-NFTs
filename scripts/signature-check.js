2bd81a7c575f5a0d70a453e9cdafc967119f50c4
Alexandru-Daniel Tiganus <alexandu24@gmail.com>
2023-06-15 01:34:37 +0300

    add sv

--- scripts/signature-check.js
+++ scripts/signature-check.js
@@ -1,64 +1,49 @@
-const { ethers } = require("hardhat");
-const hre = require("hardhat");
+const express = require("express");
+const { ethers } = require("ethers");
+const app = express();
+app.use(express.json());
 
-async function main() {
+// Define a list of allowlisted wallets
+const allowlistedAddresses = [
+  "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+  "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc",
+  "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+  "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",
+  "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc",
+];
 
-    // Define a list of allowlisted wallets
-    const allowlistedAddresses = [
-        '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
-        '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc',
-        '0x90f79bf6eb2c4f870365e785982e1f101e93b906',
-        '0x15d34aaf54267db7d7c367839aaf71a00a2c6a65',
-        '0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc',
-    ];
+// Define wallet that will be used to sign messages
+const walletAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+const privateKey =
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const signer = new ethers.Wallet(privateKey);
 
-    // Select an allowlisted address to mint NFT
-    const selectedAddress = '0x90f79bf6eb2c4f870365e785982e1f101e93b906'
+app.post("/check-address", async (req, res) => {
+  const { address } = req.body;
 
-    // Define wallet that will be used to sign messages
-    const walletAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
-    const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-    const signer = new ethers.Wallet(privateKey);
-    console.log("Wallet used to sign messages: ", signer.address, "\n");
+  // Check if address is in allowlist
+  if (allowlistedAddresses.includes(address)) {
+    // Compute message hash
+    const messageHash = ethers.id(address);
 
-    let messageHash, signature;
+    // Sign the message hash
+    let messageBytes = ethers.getBytes(messageHash);
+    const signature = await signer.signMessage(messageBytes);
 
-    // Check if selected address is in allowlist
-    // If yes, sign the wallet's address
-    if (allowlistedAddresses.includes(selectedAddress)) {
-        console.log("Address is allowlisted! Minting should be possible.");
-
-        // Compute message hash
-        messageHash = ethers.utils.id(selectedAddress);
-        console.log("Message Hash: ", messageHash);
-
-        // Sign the message hash
-        let messageBytes = ethers.utils.arrayify(messageHash);
-        signature = await signer.signMessage(messageBytes);
-        console.log("Signature: ", signature, "\n");
-    }
-
-    const factory = await hre.ethers.getContractFactory("NFTAllowlist");
-    const [owner, address1, address2] = await hre.ethers.getSigners();
-    const contract = await factory.deploy();
-
-    await contract.deployed();
-    console.log("Contract deployed to: ", contract.address);
-    console.log("Contract deployed by (Owner/Signing Wallet): ", owner.address, "\n");
-
-    recover = await contract.recoverSigner(messageHash, signature);
-    console.log("Message was signed by: ", recover.toString());
-
-    let txn;
-    txn = await contract.preSale(2, messageHash, signature);
-    await txn.wait();
-    console.log("NFTs minted successfully!");
-
-}
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
+    res.json({
+      status: "success",
+      signature,
     });
+  } else {
+    res.json({
+      status: "error",
+      message: "Address not in the allowlist",
+    });
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
