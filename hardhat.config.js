5054ce881fe0fa0c2410a4cc2c2a42d5957d627e
Iuga Bogdan <iugabogdan13@gmail.com>
2023-06-19 00:34:50 +0300

    deploy to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomicfoundation/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.18",
-};
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
